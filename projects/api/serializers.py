c5ed37d2f8d646a747897bb884dd55940760c414
zackckpetersen <zackcpetersen@gmail.com>
2021-06-02 17:08:59 -0600

    updating serializers for all needed fields

--- projects/api/serializers.py
+++ projects/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from projects.models import Project, ProjectDescription, ProjectImage, ProjectTag
+from projects.models import Project, ProjectDescription, ProjectImage, ProjectTag, Tag
 
 
 class ProjectDescriptionSerializer(serializers.ModelSerializer):
@@ -16,17 +16,32 @@ class ProjectImageSerializer(serializers.ModelSerializer):
 
 
 class ProjectTagSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
     class Meta:
         model = ProjectTag
         fields = '__all__'
+
+    def get_name(self, project_tag):
+        return project_tag.tag.name
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     images = ProjectImageSerializer(many=True)
     descriptions = ProjectDescriptionSerializer(many=True)
     tags = ProjectTagSerializer(many=True)
+    all_tags = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
         fields = ['id', 'name', 'user', 'source', 'live_url',
-                  'images', 'descriptions', 'tags']
+                  'images', 'descriptions', 'tags', 'all_tags']
+
+    def get_all_tags(self, project):
+        return project.tags.values_list('tag__name', flat=True)
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
--- projects/api/urls.py
+++ projects/api/urls.py
@@ -5,3 +5,4 @@ from projects.api import viewsets as project_views
 router = routers.DefaultRouter()
 
 router.register(r'projects', project_views.ProjectViewSet)
+router.register(r'tags', project_views.TagViewSet)
