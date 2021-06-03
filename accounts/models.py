4ece36a5fccf2f6799c4740498de4584b41ac979
zackckpetersen <zackcpetersen@gmail.com>
2021-06-02 21:38:38 -0600

    removing user fk from contactrequest

--- accounts/models.py
+++ accounts/models.py
@@ -44,7 +44,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class ContactRequest(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.first())
     name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
