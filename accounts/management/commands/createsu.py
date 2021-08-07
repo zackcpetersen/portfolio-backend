75928c249111ff9143e52d1d036bcd1081f3ee9e
zackckpetersen <zackcpetersen@gmail.com>
2021-08-07 13:28:47 -0600

    fixing mgmt command

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = 'Creates Organization for Django Tenant'
 
     def handle(self, *args, **kwargs):
-        if not User.objects.get(email='zackcpetersen@gmail.com'):
+        if not User.objects.filter(email='zackcpetersen@gmail.com').first():
             User.objects.create_superuser('zackcpetersen@gmail.com', 'Zack', 'Petersen', 'admin')
             self.stdout.write(self.style.SUCCESS(
                 'Successfully created superuser for zackcpetersen@gmail.com'))
