431944fd474b357e27a36bd5788f460f58a8d68f
zackckpetersen <zackcpetersen@gmail.com>
2022-03-05 12:45:49 -0700

    removing cors allowed origins from base.py

--- portfolio_backend/settings/base.py
+++ portfolio_backend/settings/base.py
@@ -160,9 +160,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:8080']
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
