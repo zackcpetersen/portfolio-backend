93db0150d3ce0dfc8634635870cc186a07c40eae
zackckpetersen <zackcpetersen@gmail.com>
2022-03-30 09:01:49 -0600

    changing presentation title to name

--- presentations/models.py
+++ presentations/models.py
@@ -6,15 +6,15 @@ from accounts.models import User
 
 class Presentation(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    title = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     order = models.PositiveSmallIntegerField(default=99)
     image = models.ImageField(upload_to='presentation-images')
     slug = models.SlugField(editable=False, unique=True)
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.name}'
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(self.name)
         super(Presentation, self).save(*args, **kwargs)
