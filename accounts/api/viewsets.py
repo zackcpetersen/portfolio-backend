316937a19e98697e6efad28c994f81adab3fb0f8
zackckpetersen <zackcpetersen@gmail.com>
2022-04-08 09:18:42 -0600

    making contact view allowany

--- accounts/api/viewsets.py
+++ accounts/api/viewsets.py
@@ -1,10 +1,12 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import AllowAny
 
 from accounts.api.serializers import ContactRequestSerializer, UserSerializer
 from accounts.models import ContactRequest, User
 
 
 class ContactRequestViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
     queryset = ContactRequest.objects.all()
     serializer_class = ContactRequestSerializer
 
